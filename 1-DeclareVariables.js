7d8093f341b71b696c7c49917d50a1f898692786
eda.bezek <eda_bezek@hotmail.com>
2022-08-01 13:47:00 +0300

    initial commit

---
+++ 1-DeclareVariables.js
@@ -0,0 +1,18 @@
+/*8 data types provides:
+undefined : something has a not defined,we have a variable and havent set to be any variables yet
+null : nothing 
+boolean
+string
+symbol : a symbol is an immutable primitive value that is uniq
+number 
+object : can store a lot of different key value pairs
+*/
+var myName="Ed"
+myName=8
+let ourName="freecodecamp"
+const pi=3.14
+/*var is going to be able to be used throughout your whole program 
+let only be used within the scope of where you declare that 
+const it can never change
+
+var it can be change*/
---
+++ 2-StoringValuesWithTheAssignmentOperator.js
@@ -0,0 +1,17 @@
+/*There is a difference between declaring variables and assigning variables 
+ assigning operator is = */
+
+var a; //declaring
+var b=2; //declaring and assigning 
+
+console.log(a) 
+
+a=7;//i didnt have to declare because it was already declare
+
+b=a; //a assigned to b
+
+console.log(a)//see things in the console
+
+//we can see at first console.log null,second 7
+
+/*Initializing Variables with the Assignment Operator is = */
